74459def1b57c9eafb9d159a980d59a4e7f063fe
yevhenii-sulim <88433785+yevhenii-sulim@users.noreply.github.com>
2025-01-28 19:46:50 +0200

    Revert "added path for imports from relative to absolute"

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
-
-if (!process.env.IS_TS_MODE) {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  require('module-alias/register');
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
