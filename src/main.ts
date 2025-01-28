9855cc9ae9fb4a3a27dd53bf75c7aef5197531bd
yevhenii-sulim <bees127@ukr.name>
2025-01-28 18:51:51 +0200

    added path for imports from relative to absolute

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+if (!process.env.IS_TS_MODE) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('module-alias/register');
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
