b08e1201b0e492194180a5f212513edfccaa5452
Volodymyr Basok <volodymyrquo@gmail.com>
2025-02-04 19:08:10 +0200

    Add global prefix

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap(): Promise<void> {
     .addTag('Store')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
+  SwaggerModule.setup('docs', app, documentFactory);
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
