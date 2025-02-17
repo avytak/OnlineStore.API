3138c7e9179bc74f7717849544b15a4760c10f3a
Volodymyr Basok <volodymyrquo@gmail.com>
2025-02-17 09:49:12 +0200

    Add sripts to package.json

--- src/database/drizzle.module.ts
+++ src/database/drizzle.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { DrizzleDB } from '@app/database/drizzle';
+import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
 import * as schema from './schema';
 
 export const DRIZZLE = Symbol('drizzle-connection');
+config({ path: `.env.${process.env.NODE_ENV || 'production'}` });
 
 @Module({
   providers: [
