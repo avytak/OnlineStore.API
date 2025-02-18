02a90a05d0426a9548c4f2ec9ac530377ddfa3f5
yevhenii-sulim <bees127@ukr.name>
2025-02-18 23:00:46 +0200

    create user-address module and added schemas for arders and user-address

--- drizzle/0000_needy_bromley.sql
+++ drizzle/0000_needy_bromley.sql
@@ -1,18 +1,15 @@
+CREATE TYPE "public"."statuses" AS ENUM('processing', 'sent', 'delivered', 'cancelled');--> statement-breakpoint
 CREATE TABLE "categories" (
 
 );
 --> statement-breakpoint
 CREATE TABLE "orders" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "orders_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"total_price" numeric(5, 0) NOT NULL,
-	"status" varchar(20) NOT NULL,
-	"first_name" varchar(20) NOT NULL,
-	"last_name" varchar(20) NOT NULL,
-	"phone" varchar(20) NOT NULL,
-	"shipping-address" text NOT NULL,
+	"status" "statuses" DEFAULT 'processing',
 	"payment_type" varchar(255) NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now(),
-	"updated_at" timestamp with time zone DEFAULT now()
+	"updated_at" timestamp with time zone DEFAULT now(),
+	"userId" integer NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "product" (
@@ -27,6 +24,16 @@ CREATE TABLE "product" (
 	CONSTRAINT "product_id_unique" UNIQUE("id")
 );
 --> statement-breakpoint
+CREATE TABLE "userAddress" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "userAddress_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"street" varchar(100),
+	"city" varchar(100),
+	"state" varchar(100),
+	"postal_code" varchar(20),
+	"country" varchar(100)
+);
+--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"first_name" varchar(20) NOT NULL,
@@ -35,3 +42,6 @@ CREATE TABLE "users" (
 	"email" varchar(20) NOT NULL,
 	"password" varchar(70) NOT NULL
 );
+--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "userAddress" ADD CONSTRAINT "userAddress_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
